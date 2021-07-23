caaabefc711995d13c7bdfe3960b69b3bc18600f
Onebone <nbakobejames@gmail.com>
2021-07-24 02:31:06 +0800

    v1.0

--- genshin_bot.go
+++ genshin_bot.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"time"
+
 	"github.com/roylee0704/gron"
-	"github.com/roylee0704/gron/xtime"
 
 	"genshin/account"
 	"genshin/bot_func"
@@ -12,7 +13,7 @@ func main() {
 	account.Accs.Init()
 	_, updates := bot_func.Bot_init()
 	cron := gron.New()
-	cron.AddFunc(gron.Every(1*xtime.Day).At("00:00"), account.Accs.Signin)
+	cron.AddFunc(gron.Every(1*time.Hour), account.Accs.Signin)
 	cron.Start()
 
 	for update := range updates {
